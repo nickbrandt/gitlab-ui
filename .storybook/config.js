2c6290cc35e628eddf02d349781036fe2775528f
Adriel Santiago <adriel@gitlab.com>
2018-08-15 19:46:47 -0400

    added bootstrap-vue pagination

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,14 +3,17 @@ import { configure } from '@storybook/vue';
 import Vue from 'vue';
 
 // Import your custom components.
+import pagination from '../components/base/pagination.vue';
 import progressBar from '../components/base/progress_bar.vue';
 
 // Register custom components.
+Vue.component('gl-pagination', pagination);
 Vue.component('gl-progress-bar', progressBar);
 
 function loadStories() {
   // You can require as many stories as you need.
   require('../stories');
+  require('../stories/pagination.js');
 }
 
 configure(loadStories, module);
