51ee01807b7c935248ce0078d7efa11dc2c28b42
Adriel Santiago <adriel@gitlab.com>
2018-08-19 21:24:52 -0400

    clean up formatting and excess code

--- components/base/pagination.vue
+++ components/base/pagination.vue
@@ -5,46 +5,34 @@
  * https://bootstrap-vue.js.org/docs/components/pagination
  * https://bootstrap-vue.js.org/docs/components/pagination-nav
  */
-import bPagination from 'bootstrap-vue/es/components/pagination/pagination';
-import bPaginationNav from 'bootstrap-vue/es/components/pagination-nav/pagination-nav';
+import BPaginationNav from "bootstrap-vue/es/components/pagination-nav/pagination-nav";
 
 export default {
   components: {
-    bPagination,
-    bPaginationNav
+    BPaginationNav
   },
   computed: {
-    defaultAttrs () {
+    defaultAttrs() {
       return {
         limit: 11
-      }
+      };
     },
-    isTypeNav () {
-      return typeof this.$attrs['link-gen'] === 'function'
-    },
-    mergedAttrs () {
+    mergedAttrs() {
       for (const attr in this.defaultAttrs) {
         if (!this.$attrs[attr]) {
-          this.$attrs[attr] = this.defaultAttrs[attr]
+          this.$attrs[attr] = this.defaultAttrs[attr];
         }
       }
-      if (!this.$attrs['hide-goto-end-buttons']) {
-        this.$attrs['hide-goto-end-buttons'] = this.$attrs['number-of-pages'] < this.$attrs.limit
+      if (!this.$attrs["hide-goto-end-buttons"]) {
+        this.$attrs["hide-goto-end-buttons"] =
+          this.$attrs["number-of-pages"] < this.$attrs.limit;
       }
-      return this.$attrs
-    },
-    pagination () {
-      return this.isTypeNav
-        ? bPaginationNav
-        : bPagination
+      return this.$attrs;
     }
-  },
+  }
 };
 </script>
 
 <template>
-  <component
-    :is="pagination"
-    v-bind="mergedAttrs"
-  />
+  <BPaginationNav v-bind="mergedAttrs" />
 </template>
--- stories/pagination.js
+++ stories/pagination.js
@@ -1,27 +1,21 @@
-import { storiesOf } from '@storybook/vue';
+import { storiesOf } from "@storybook/vue";
 
 const data = () => {
   return {
     currentPage: 3
-  }
-}
+  };
+};
 
-storiesOf('pagination/with navigation', module)
-  .add('using link generator', () => ({
+storiesOf("pagination", module)
+  .add("default", () => ({
     data,
-    template: '<gl-pagination :link-gen="pageNum => `#`" :number-of-pages="20" :value="currentPage" />'
+    template: `<gl-pagination :link-gen="pageNum => '#'" :number-of-pages="20" :value="currentPage" />`
   }))
-
-storiesOf('pagination/without navigation', module)
-  .add('default', () => ({
+  .add("small", () => ({
     data,
-    template: '<gl-pagination :navOnChange="false" :total-rows="100" :per-page="10" :value="currentPage" />'
+    template: `<gl-pagination size="sm" :link-gen="pageNum => '#'" :number-of-pages="20" :value="currentPage" />`
   }))
-  .add('small', () => ({
+  .add("large", () => ({
     data,
-    template: '<gl-pagination :navOnChange="false" size="sm" :total-rows="100" :per-page="10" :value="currentPage" />'
-  }))
-  .add('large', () => ({
-    data,
-    template: '<gl-pagination size="lg" :navOnChange="false" :total-rows="100" :per-page="10" :value="currentPage" />'
+    template: `<gl-pagination size="lg" :link-gen="pageNum => '#'" :number-of-pages="20" :value="currentPage" />`
   }));
