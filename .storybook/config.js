9d6836b53c01dbce32862dda3cf7260f1561235f
Clement Ho <clemmakesapps@gmail.com>
2018-09-07 16:45:02 +0000

    feat: add modal components

--- .storybook/config.js
+++ .storybook/config.js
@@ -5,13 +5,22 @@ import Vue from 'vue';
 
 // Import your custom components.
 import progressBar from '../components/base/progress_bar.vue';
+import modal from '../components/base/modal.vue';
+
+// Import your custom directives.
+import vModal from '../directives/modal';
 
 // Register custom components.
 Vue.component('gl-progress-bar', progressBar);
+Vue.component('gl-modal', modal);
+
+// Register custom directives.
+Vue.directive('gl-modal', vModal);
 
 function loadStories() {
   // You can require as many stories as you need.
   require('../stories/base/progress_bar');
+  require('../stories/base/modal');
 }
 
 setOptions({
