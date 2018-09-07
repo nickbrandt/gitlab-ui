d1278c71bd8d93bd921c83e089cde805a5eaffc5
Adriel Santiago <adriel@gitlab.com>
2018-09-07 17:08:28 -0400

    add knobs to pagination story

--- stories/base/pagination.js
+++ stories/base/pagination.js
@@ -1,15 +1,37 @@
 import { storiesOf } from "@storybook/vue";
+import { withKnobs, number } from '@storybook/addon-knobs';
 
-const data = () => ({
-  change: () => {},
-  page: 3,
-  perPage: 10,
-  totalItems: 200,
-});
+function generateProps({
+  page = 3,
+  perPage = 10,
+  totalItems = 200,
+} = {}) {
+  return {
+    change: {
+      type: Function,
+      default: (current, past) => console.log(
+        `switched from ${past} to ${current}`,
+      ),
+    },
+    page: {
+      type: Number,
+      default: number('current page', page),
+    },
+    perPage: {
+      type: Number,
+      default: number('per page', perPage),
+    },
+    totalItems: {
+      type: Number,
+      default: number('total items', totalItems),
+    },
+  };
+};
 
 storiesOf("pagination", module)
+  .addDecorator(withKnobs)
   .add("default", () => ({
-    data,
+    props: generateProps(),
     template: `<gl-pagination
       :change="change"
       :page="page"
@@ -18,7 +40,7 @@ storiesOf("pagination", module)
       />`,
   }))
   .add("small", () => ({
-    data,
+    props: generateProps(),
     template: `<gl-pagination
       size="sm"
       :change="change"
@@ -28,7 +50,7 @@ storiesOf("pagination", module)
       />`,
   }))
   .add("large", () => ({
-    data,
+    props: generateProps(),
     template: `<gl-pagination
       size="lg"
       :change="change"
