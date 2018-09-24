9fd8db71d11d3c1e88e1c7536796609c2aaa600f
Clement Ho <clemmakesapps@gmail.com>
2018-09-24 08:08:51 +0000

    docs: add docs for loading icon

--- stories/utils/documented_stories.js
+++ stories/utils/documented_stories.js
@@ -1,11 +1,30 @@
 import { storiesOf } from '@storybook/vue';
 import { withDocs } from 'storybook-readme';
 
+const withCustomPreview = withDocs({
+  PreviewComponent: {
+    data() {
+      return {
+        // Style the preview component container
+        // Default container forcefully centers the preview element
+        styles: {
+          padding: '50px 35px',
+          margin: '16px 0',
+          border: '1px dashed rgb(229, 229, 229)',
+        },
+      };
+    },
+    template: `<div v-bind:style="styles"><slot></slot></div>`,
+  },
+  // Disable default footer's dashed bottom border
+  FooterComponent: {},
+});
+
 function documentedStoriesOf(storyName, readme) {
   const story = storiesOf(storyName, module);
 
   if (process.env.NODE_ENV !== 'test') {
-    story.addDecorator(withDocs(readme));
+    story.addDecorator(withCustomPreview(readme));
   }
 
   return story;
