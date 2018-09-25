c8743d3d17c77f89322127a246bebdd62b7bf79b
Mike Greiling <mike@pixelcog.com>
2018-09-25 23:18:53 +0000

    fix: Update rollup config with single point of entry

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import glob from 'glob';
 
 export default glob
   .sync('+(components|directives)/**/*.+(js|vue)')
+  .concat('index.js')
   .map((input) => {
     const outputFilename = input
       .replace(/\.(vue|js)$/, '');
