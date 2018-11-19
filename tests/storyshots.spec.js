98bf2efc344f07b2d0ae8a6a22b168c6d33c6861
Tim Zallmann <tzallmann@gitlab.com>
2018-11-19 09:34:12 +0000

    Resolve "Loading icon visual diff fails on master"

--- tests/storyshots.spec.js
+++ tests/storyshots.spec.js
@@ -1,14 +1,36 @@
 import initStoryshots from '@storybook/addon-storyshots';
 import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
 
-const beforeScreenshot = (page) => {
-  page.addStyleTag({ path: 'tests/storyshots.css' });
+const beforeScreenshot = page => {
+  // Fixing the Animation by inlining, previous approach with external file was flaky for the animation
+  page.addStyleTag({
+    content: `
+      * {
+        -webkit-transition: none !important;
+        -moz-transition: none !important;
+        -ms-transition: none !important;
+        -o-transition: none !important;
+        transition: none !important;
+    
+        -webkit-animation: none !important;
+        -moz-animation: none !important;
+        -ms-animation: none !important;
+        -o-animation: none !important;
+        animation: none !important;
+      }
+    `,
+  });
 };
+
+const getGotoOptions = () => ({
+  waitUntil: 'networkidle0',
+});
 
 initStoryshots({
   suite: 'Image storyshots',
   test: imageSnapshot({
     storybookUrl: `file:///${__dirname}/../storybook`,
     beforeScreenshot,
+    getGotoOptions,
   }),
 });
