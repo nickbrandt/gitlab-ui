a003fa68f3bf318b5e3cf4ec1ef702a16a239284
Clement Ho <ClemMakesApps@gmail.com>
2018-11-21 13:59:36 -0600

    Try inline style

--- tests/storyshots.spec.js
+++ tests/storyshots.spec.js
@@ -3,7 +3,23 @@ import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
 
 const beforeScreenshot = page => {
   // Fixing the Animation by inlining, previous approach with external file was flaky for the animation
-  page.addStyleTag({ path: 'tests/storyshots.css' });
+  page.addStyleTag({
+    content: `
+      * {
+        -webkit-transition: none !important;
+        -moz-transition: none !important;
+        -ms-transition: none !important;
+        -o-transition: none !important;
+        transition: none !important;
+
+        -webkit-animation: none !important;
+        -moz-animation: none !important;
+        -ms-animation: none !important;
+        -o-animation: none !important;
+        animation: none !important;
+      }
+    `,
+  });
 };
 
 const getGotoOptions = () => ({
