638750b0c8e07dd107e5b663d352c25066a12a22
Tim Zallmann <tzallmann@gitlab.com>
2018-11-22 14:53:14 +0000

    fix: Resets the animation of Skeleton Loader for more stable CI

--- tests/storyshots.spec.js
+++ tests/storyshots.spec.js
@@ -1,24 +1,11 @@
 import initStoryshots from '@storybook/addon-storyshots';
 import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
+import { getResetAnimationsCSS } from './test_utils';
 
 const beforeScreenshot = page => {
   // Fixing the Animation by inlining, previous approach with external file was flaky for the animation
   page.addStyleTag({
-    content: `
-      * {
-        -webkit-transition: none !important;
-        -moz-transition: none !important;
-        -ms-transition: none !important;
-        -o-transition: none !important;
-        transition: none !important;
-    
-        -webkit-animation: none !important;
-        -moz-animation: none !important;
-        -ms-animation: none !important;
-        -o-animation: none !important;
-        animation: none !important;
-      }
-    `,
+    content: getResetAnimationsCSS(),
   });
 };
 
