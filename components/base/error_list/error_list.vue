93d8793bd512be0a4baa2a4f02a8567256bd6482
Jose Vargas <jvargas@gitlab.com>
2018-12-13 16:50:16 +0100

    Stylize rows and headers

--- components/base/error_list/error_list.vue
+++ components/base/error_list/error_list.vue
@@ -1,4 +1,5 @@
 <script>
+import { GlLink } from '../../../index';
 export default {
   props: {
     errors: {
@@ -6,24 +7,48 @@ export default {
       required: true,
     },
   },
+  components: {
+    GlLink,
+  },
 };
 </script>
 <template>
   <div>
-    <ul v-for="error in errors" :key="error.id" class="content-list issues-list issuable-list">
-      <li>
-        <div class="issue-box">
-          <div class="issuable-info-container">
-            <div class="issuable-main-info">
-              <div class="issue-title title">
-                <span class="issue-title-text">
-                  {{ error.title }}
-                </span>
+    <div class="sentry-header-box">
+      <div class="sentry-description-header">
+        Open Errors
+      </div>
+      <div class="sentry-events-header">
+        Events
+      </div>
+      <div class="sentry-users-header">
+        Users      
+      </div>
+      <div class="sentry-last-seen-header">
+        Last seen
+      </div>
+    </div>
+    <div class="issues-holder">
+      <ul class="content-list issues-list issuable-list">
+        <li v-for="error in errors" :key="error.id" class="issue">
+          <div class="issue-box">
+            <div class="issuable-info-container">
+              <div class="sentry-description">
+                <div class="issue-title title">
+                  <span class="issue-title-text">
+                    <gl-link :href="error.external_url">{{ error.title.trim() }}</gl-link>
+                  </span>
+                  <span class="sentry-culprit">/ {{ error.culprit }}</span>
+                </div>
+                {{ error.message }}
               </div>
+              <div class="sentry-events">{{ error.count }}</div>
+              <div class="sentry-users">{{ error.user_count }}</div>
+              <div class="sentry-lastseen">{{ error.last_seen }}</div>
             </div>
           </div>
-        </div>
-      </li>
-    </ul>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
