a0e8a2458dc86ca1b3b9a1ea2b287335c90e62e1
Sam Beckham <sbeckham@gitlab.com>
2019-02-22 16:20:04 +0000

    Allows `no-caret` to be used in gl-dropdown
    The `no-caret` param exists in bootstrap-vue but we override it to
    provide our own in GitLab UI.
    This Allows us to use it again.
    
    - Conditionally renders the chevron depending on the param
    - Creates and example and page

---
+++ components/base/dropdown/examples/dropdown.no_caret.example.vue
@@ -0,0 +1,7 @@
+<template>
+  <gl-dropdown text="Some dropdown" no-caret>
+    <gl-dropdown-item>First item</gl-dropdown-item>
+    <gl-dropdown-item>Second item</gl-dropdown-item>
+    <gl-dropdown-item>Last item</gl-dropdown-item>
+  </gl-dropdown>
+</template>
