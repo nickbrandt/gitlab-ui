f1696ebc5b67f5f4168bc0947e2527bc6085bd36
Lukas Eipert <leipert@gitlab.com>
2019-03-12 15:14:55 +0100

    docs(storybook): Adding a nice GitLab logo for branding

--- .storybook/config.js
+++ .storybook/config.js
@@ -11,7 +11,8 @@ addParameters({
   options: {
     theme: create({
       base: 'light',
-      brandTitle: 'gitlab-ui',
+      brandTitle: 'GitLab UI',
+      brandImage: 'https://about.gitlab.com/images/press/logo/png/gitlab-logo-gray-rgb.png',
       brandUrl: 'https://gitlab.com/gitlab-org/gitlab-ui',
     }),
     isFullscreen: false,
