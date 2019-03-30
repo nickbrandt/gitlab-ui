0a370c788a74ce9dc5caaae90b65596ba3d6392b
Luke Bennett <lukeeeebennettplus@gmail.com>
2019-03-30 18:42:27 +0000

    Add GlSteppablePagination

---
+++ components/base/pagination/index.js
@@ -0,0 +1,2 @@
+export { default as GlPagination } from './pagination.vue';
+export { default as GlSteppablePagination } from './steppable_pagination.vue';
---
+++ components/base/pagination/steppable_pagination.vue
@@ -0,0 +1,44 @@
+<script>
+import GlPagination from './pagination.vue';
+
+export default {
+  components: {
+    GlPagination,
+  },
+  props: {
+    change: {
+      type: Function,
+      required: true,
+    },
+    page: {
+      type: Number,
+      required: true,
+    },
+    hasNextPage: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      currentPage: this.page,
+    };
+  },
+  computed: {
+    totalItems() {
+      return this.hasNextPage ? this.page + 1 : this.page;
+    },
+  },
+};
+</script>
+
+<template>
+  <gl-pagination
+    :page="currentPage"
+    v-bind="$attrs"
+    :total-items="totalItems"
+    :per-page="1"
+    :change="change"
+  />
+</template>
