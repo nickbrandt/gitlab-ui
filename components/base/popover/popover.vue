71d0d0b6e71df839f23c4dfdb690716304e520ff
Martin Wortschack <mwortschack@gitlab.com>
2019-05-24 14:56:46 +0000

    Resolve "GlPopover doesn't support custom CSS classes (BS Popover Issue)"

--- components/base/popover/popover.vue
+++ components/base/popover/popover.vue
@@ -6,11 +6,28 @@ export default {
     BPopover,
   },
   inheritAttrs: false,
+  props: {
+    cssClasses: {
+      type: Array,
+      required: false,
+      default: () => [],
+    },
+  },
+  methods: {
+    onPopoverShow() {
+      // workaround for appending a custom class to the bs popover which cannot be done via props
+      // see https://github.com/bootstrap-vue/bootstrap-vue/issues/1983
+      if (this.cssClasses.length > 0) {
+        // eslint-disable-next-line no-underscore-dangle
+        this.$refs.bPopover._toolpop.getTipElement().classList.add(...this.cssClasses);
+      }
+    },
+  },
 };
 </script>
 
 <template>
-  <b-popover v-bind="$attrs">
+  <b-popover ref="bPopover" v-bind="$attrs" @show="onPopoverShow">
     <template slot="title">
       <slot name="title"></slot>
     </template>
