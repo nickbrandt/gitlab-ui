d5c18a319ac113793909dbd6b38fc59372f1f6d3
Enrique Alcántara <ealcantara@gitlab.com>
2019-05-28 07:59:21 +0000

    Add knobs for popover placement and title properties

--- components/base/popover/popover.stories.js
+++ components/base/popover/popover.stories.js
@@ -1,21 +1,41 @@
-import { withKnobs } from '@storybook/addon-knobs';
+import { withKnobs, select, text } from '@storybook/addon-knobs';
 import documentedStoriesOf from '../../../utils/documented_stories';
+import { popoverPlacements } from '../../../utils/constants';
 
 const template = `
   <div>
-    <gl-button id="pop-top">Top</gl-button>
+    <gl-button id="pop-top">{{placement}}</gl-button>
     <gl-popover target="pop-top"
-      placement="top"
-      title="Popover!"
       triggers="hover focus"
-      content="Placement Top"
+      :title="title"
+      :placement="placement"
+      :content="content"
       show
       />
   </div>
   `;
 
+function generateProps({ placement = popoverPlacements.top, title = 'Popover!' } = {}) {
+  return {
+    placement: {
+      type: String,
+      default: select('placement', popoverPlacements, placement),
+    },
+    title: {
+      type: String,
+      default: text('title', title),
+    },
+  };
+}
+
 documentedStoriesOf('base|popover', '')
   .addDecorator(withKnobs)
   .add('default', () => ({
     template,
+    props: generateProps(),
+    computed: {
+      content() {
+        return `Placement ${this.placement}`;
+      },
+    },
   }));
