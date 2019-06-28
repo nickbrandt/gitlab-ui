c32e888f88761eb18d1dcb045ff0bfc5b4760c48
Enrique Alcantara <ealcantara@gitlab.com>
2019-06-28 09:42:09 -0400

    docs: Wrap dropdown examples in unstyled lists

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,7 +3,6 @@ import { create } from '@storybook/theming';
 
 const req = require.context('../components', true, /\.stories\.js$/);
 
-import '../styles/sb_specifics.css';
 import '../scss/components/gitlab_ui_special.scss';
 
 function loadStories() {
