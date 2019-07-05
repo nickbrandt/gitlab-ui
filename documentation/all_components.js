d2e3aaa661549e35e4bde79c8d8cfd142eb0ec13
Simon Knox <psimyn@gmail.com>
2019-07-05 22:22:25 +1000

    Move documentation components to design.gitlab

--- documentation/all_components.js
+++
@@ -1,7 +0,0 @@
-import * as components from '../index';
-import * as chartComponents from '../charts';
-
-export const gitlabComponents = { ...components, ...chartComponents };
-
-export const componentValidator = componentName =>
-  Object.keys(gitlabComponents).includes(componentName);
