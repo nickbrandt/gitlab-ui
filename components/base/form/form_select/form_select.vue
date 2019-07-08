a3c8e00d90e8937fb7eb743c7c226cdb137a7cfa
Lukas Eipert <leipert@gitlab.com>
2019-07-08 07:58:43 +0200

    refactor(styles): Move component styles to SCSS files
    Having styles in Vue components means that they are added during runtime
    by a script which injects them. This is bad for two reasons:
    
    1. We are effectivly introducing side-effects, especially with
    non-scoped styles. This makes the package not tree-shakable
    
    2. We cannot make use of our SCSS pipeline

--- components/base/form/form_select/form_select.vue
+++ components/base/form/form_select/form_select.vue
@@ -9,19 +9,7 @@ export default {
 };
 </script>
 <template>
-  <b-form-select v-bind="$attrs" v-on="$listeners">
+  <b-form-select class="gl-form-select" v-bind="$attrs" v-on="$listeners">
     <slot v-for="slot in Object.keys($slots)" :slot="slot" :name="slot"></slot>
   </b-form-select>
 </template>
-<style scoped>
-/*
-This is a temporary measure to ensure custom select styles are not affected by our
-lack of an autoprefixer tool for compiling bootstrap.
-
-TODO: Remove once bootstrap is included in gitlab-ui or we include an autoprefixer
-on gitlab-ce/ee
-*/
-.custom-select {
-  -webkit-appearance: none;
-}
-</style>
