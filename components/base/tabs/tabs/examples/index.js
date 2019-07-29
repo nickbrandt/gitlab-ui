72c3b147fe595ba9a367ade59ccaa73897192819
Enrique Alcantara <ealcantara@gitlab.com>
2019-07-29 09:46:00 -0400

    Add styles only example

--- components/base/tabs/tabs/examples/index.js
+++ components/base/tabs/tabs/examples/index.js
@@ -3,7 +3,8 @@ import DisabledTabExample from './tabs.disabled.example.vue';
 import CustomTitleExample from './tabs.custom_title.example.vue';
 import ContentlessTabExample from './tabs.contentless_tab.example.vue';
 import NoTabsExample from './tabs.no_tabs.example.vue';
-import TabsWithCounterBadges from './tabs.counterbadges.example.vue';
+import TabsWithCounterBadgesExample from './tabs.counterbadges.example.vue';
+import TabsStylesOnlyExample from './tabs.styles_only.example.vue';
 
 export default [
   {
@@ -42,7 +43,12 @@ export default [
       {
         id: 'tabs-with-counter-badges',
         name: 'Tabs with counter badges',
-        component: TabsWithCounterBadges,
+        component: TabsWithCounterBadgesExample,
+      },
+      {
+        id: 'tabs-styles-only',
+        name: 'Tabs styles only',
+        component: TabsStylesOnlyExample,
       },
     ],
   },
