c33aeac8462a6cc2771fc688dd33902f3396b1f0
Laura Montemayor <lmontemayor@gitlab.com>
2019-07-30 22:44:35 +0000

    Updates structure of breadcrumb to override boostrap styles
    * added styles for breadcrumbs
    * adds new utility class for margin
    * adds mixin for string truncation
    
    Updates styles to use utility classes

--- components/base/breadcrumb/breadcrumb.vue
+++ components/base/breadcrumb/breadcrumb.vue
@@ -1,16 +1,38 @@
 <script>
 import BBreadcrumb from 'bootstrap-vue/src/components/breadcrumb/breadcrumb';
+import BBreadcrumbItem from 'bootstrap-vue/src/components/breadcrumb/breadcrumb-item';
 
 export default {
   components: {
     BBreadcrumb,
+    BBreadcrumbItem,
   },
   inheritAttrs: false,
+  props: {
+    items: {
+      type: Array,
+      required: true,
+      default: () => [{ text: '', href: '' }],
+      validator: links => links.every(link => Object.keys(link).includes('text', 'href')),
+    },
+  },
 };
 </script>
 <template>
-  <b-breadcrumb
-    v-bind="$attrs"
-    v-on="$listeners"
-  />
+  <div class="gl-breadcrumbs">
+    <b-breadcrumb
+      class="gl-breadcrumb-list"
+      v-bind="$attrs"
+      v-on="$listeners"
+    >
+      <slot name="avatar"></slot>
+      <template v-for="(item, index) in items">
+        <b-breadcrumb-item :key="index" :text="item.text" :href="item.href" />
+          <span v-if="index != items.length -1" :key="`index ${item.text}`"
+            class="gl-breadcrumb-separator">
+            <slot name="caret"></slot>
+          </span>
+      </template>
+    </b-breadcrumb>
+  </div>
 </template>
--- components/base/breadcrumb/examples/breadcrumb.basic.example.vue
+++ components/base/breadcrumb/examples/breadcrumb.basic.example.vue
@@ -1,5 +1,14 @@
 <template>
-  <gl-breadcrumb :items="items"/>
+  <gl-breadcrumb :items="items">
+    <template #avatar>
+      <img class="gl-breadcrumb-avatar-tile" src="https://assets.gitlab-static.net/uploads/-/system/group/avatar/9970/logo-extra-whitespace.png?width=15" width="15" height="15" />
+    </template>
+    <template #caret>
+      <svg viewBox="0 0 16 16">
+        <path d="M9.9 8.064L5.656 3.821A1 1 0 0 1 7.07 2.407l4.95 4.95a1 1 0 0 1 0 1.414l-4.95 4.95a1 1 0 0 1-1.414-1.414l4.242-4.243z" fill-rule="evenodd"/>
+      </svg> 
+    </template>
+  </gl-breadcrumb>
 </template>
 
 <script>
@@ -17,7 +26,7 @@ export default {
         },
         {
           text: 'Third Item',
-          active: true,
+          href: '#',
         },
       ],
     };
