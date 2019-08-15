025a68f3c4685f353da796874f28630d90497853
rhardarson <rhardarson@gitlab.com>
2019-08-15 15:38:38 +0000

    Use hard coded styles for the card

--- components/base/card/examples/card.purchase.plans.example.vue
+++ components/base/card/examples/card.purchase.plans.example.vue
@@ -1,31 +1,34 @@
+
 <template>
   <div>
-    <gl-card
-    no-body
-  >
-  <b-card-header>
-      Bronze
-  </b-card-header>
+    <gl-card no-body style="max-width: 20rem;" >
+      <b-card-header>Bronze</b-card-header>
       <b-card-body>
-    <gl-card-text>
-      $4 per user per month.
-      Billed anually at $48
-    </gl-card-text>
-
-    <p>break</p>
-    <gl-card-text>
-        Unlimited repositories
-        2,000 CI pipeline minutes/month
-        Next day support
-    </gl-card-text>
-    <gl-link href="https://about.gitlab.com/pricing/#gitlab-com">See all Bronze features</gl-link>
-    </b-card-body>
-
-    <gl-card-footer >
-        <div class="flex justify-content-end">
-        <gl-button href="#" variant="primary">Upgrade</gl-button>
+        <div class="price-per-month">
+          <div class="append-right-5">$0</div>
+          <ul class="billing-conditions">
+            <li>per user</li>
+            <li>monthly</li>
+          </ul>
         </div>
-    </gl-card-footer>
-  </gl-card>
+        <p class="subtext">Billed anually at $48</p>
+
+        <hr />
+
+        <div class="features">
+          <p>Unlimited repositories</p>
+          <p>2,000 CI pipeline minutes/month</p>
+          <p>Next day support</p>
+        </div>
+
+        <gl-link href="https://about.gitlab.com/pricing/#gitlab-com">See all Bronze features</gl-link>
+      </b-card-body>
+
+      <gl-card-footer>
+        <div class="flex justify-content-end">
+          <gl-button href="#" variant="primary">Upgrade</gl-button>
+        </div>
+      </gl-card-footer>
+    </gl-card>
   </div>
 </template>
