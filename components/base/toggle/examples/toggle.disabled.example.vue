a4054ae4a0bb799c03a7b4e54554c135f0a79071
Justin Boyson <jboyson@gitlab.com>
2019-09-04 05:34:45 +0000

    feat: Add toggle switch

---
+++ components/base/toggle/examples/toggle.disabled.example.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  data() {
+    return {
+      toggleValue: false,
+    };
+  },
+};
+</script>
+
+<template>
+  <gl-toggle v-model="toggleValue" disabled />
+</template>
