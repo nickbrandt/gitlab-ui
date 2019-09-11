ea08b6c127360f89491da2a7370926670b6f6523
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2019-09-11 14:55:33 -0400

    Fix datepicker styles

--- components/base/form/form_input/form_input.vue
+++ components/base/form/form_input/form_input.vue
@@ -10,5 +10,5 @@ export default {
 </script>
 
 <template>
-  <b-form-input v-bind="$attrs" v-on="$listeners" />
+  <b-form-input v-bind="$attrs" v-on="$listeners" class="gl-form-input" />
 </template>
