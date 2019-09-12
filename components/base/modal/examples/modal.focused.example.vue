82f5433087ca4f5ca3429e92463b43298ce48f57
pburdette <pburdette@gitlab.com>
2019-09-12 09:19:06 -0400

    Add modal spec for testing modal component

--- components/base/modal/examples/modal.focused.example.vue
+++ components/base/modal/examples/modal.focused.example.vue
@@ -3,9 +3,9 @@
     <button v-gl-modal-directive="'basic-modal-id'" type="button">
       Open modal
     </button>
-    <gl-modal @shown="focusElement" modal-id="basic-modal-id" title="Example title" no-fade>
+    <gl-modal modal-id="basic-modal-id" title="Example title" no-fade @shown="focusElement">
       This is my content
-      <gl-form-input ref="focusThis"></gl-form-input>
+      <gl-form-input ref="focusThis" />
     </gl-modal>
   </div>
 </template>
--- tests/components/base/modal.spec.js
+++ tests/components/base/modal.spec.js
@@ -45,4 +45,22 @@ describe('Modal component', () => {
     expect(findModal().props('modalClass')).toContain('gl-modal');
     expect(findModal().props('modalClass')).toContain('modal-class-override another-override');
   });
+
+  describe('modal button warning', () => {
+    it('button counter should return a number', () => {
+      const nodes = [{ tag: 'button' }];
+      expect(wrapper.vm.buttonCounter(nodes)).toBe(1);
+    });
+
+    it('returns zero if passed array of nodes without any buttons', () => {
+      const nodes = [{ tag: 'div' }];
+      expect(wrapper.vm.buttonCounter(nodes)).toBe(0);
+    });
+
+    it('console.warn button warning if number is greater than 3', () => {
+      const spy = jest.spyOn(global.console, 'warn');
+      wrapper.vm.buttonWarning(4);
+      expect(spy).toHaveBeenCalled();
+    });
+  });
 });
