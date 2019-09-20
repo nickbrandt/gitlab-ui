f3f93c528343779e7612bb8f5b0ac165aa946ff2
sstern <sstern@gitlab.com>
2019-09-20 15:03:08 -0700

    Add computed prop to form_input

--- components/base/form/form_input/form_input.vue
+++ components/base/form/form_input/form_input.vue
@@ -1,14 +1,28 @@
 <script>
 import BFormInput from 'bootstrap-vue/src/components/form-input/form-input';
+import GlButton from '../../button/button.vue';
 
 export default {
   components: {
     BFormInput,
+    GlButton,
+  },
+  data() {
+    return {
+      userInput: '',
+    }
+  },
+  computed: {
+    inputLength() {
+      return this.userInput.length;
+    }
   },
   inheritAttrs: false,
 };
 </script>
 
 <template>
-  <b-form-input v-bind="$attrs" v-on="$listeners" />
+  <div class="gl-form-input gl-form-input-placeholder">
+    <b-form-input v-model="userInput" class="gl-form-input" v-bind="$attrs" v-on="$listeners" />
+  </div>
 </template>
