1ba7034faa88b1ad9707ef79cb217c849afb59fa
sstern <sstern@gitlab.com>
2019-09-25 16:15:09 -0700

    Rm clear componnet

--- components/base/form/form_input_with_clear/examples/form_input_with_clear.text.example.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <gl-form-input-with-clear  />
-</template>
--- components/base/form/form_input_with_clear/form_input_with_clear.documentation.js
+++
@@ -1,8 +0,0 @@
-import * as description from './form_input_with_clear.md';
-// import examples from './examples/form_input_with_clear';
-
-export default {
-  description,
-  examples: {},
-  bootstrapComponent: 'b-form-input',
-};
