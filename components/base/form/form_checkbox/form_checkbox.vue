99bc0b1c6724e889822d361b542160effa16005c
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2019-09-29 09:41:57 -0400

    fix(form checkbox component): fix GitLab integration style issues

--- components/base/form/form_checkbox/form_checkbox.vue
+++ components/base/form/form_checkbox/form_checkbox.vue
@@ -23,7 +23,7 @@ export default {
     @input="$emit('input', $event)"
   >
     <slot></slot>
-    <p v-if="Boolean($slots.help)" class="help-text text-secondary mb-0">
+    <p v-if="Boolean($slots.help)" class="help-text">
       <slot name="help"></slot>
     </p>
   </b-form-checkbox>
