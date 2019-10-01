28a67ea52f2572ba790c7a27d57563e8cdcfca98
sstern <sstern@gitlab.com>
2019-10-01 13:38:27 -0700

    Add form input clearable button

--- components/base/form/form_input/form_input.vue
+++ components/base/form/form_input/form_input.vue
@@ -1,14 +1,43 @@
 <script>
 import BFormInput from 'bootstrap-vue/src/components/form-input/form-input';
+import GlButton from '../../button/button.vue';
 
 export default {
   components: {
     BFormInput,
+    GlButton,
   },
   inheritAttrs: false,
+  props: {
+    clearable: {
+      type: Boolean,
+      default: false,
+    }
+  },
+  data() {
+    return {
+      userInput: '',
+    }
+  },
+  computed: {
+    isClearable: function() {
+      return this.clearable && this.userInput.length > 0;
+    }
+  },
+  methods: {
+    clearInput: function() {
+      this.userInput = '';
+      this.$refs.input.focus();
+    }
+  }
 };
 </script>
 
 <template>
-  <b-form-input v-bind="$attrs" v-on="$listeners" />
+  <div class="gl-form-input">
+    <b-form-input ref="input" class="border-0" v-model="userInput" v-bind="$attrs" v-on="$listeners" />
+    <gl-button @click="clearInput" class="clear-button" v-if="isClearable">
+      <i aria-hidden="true" class="fa fa-times-circle" />
+    </gl-button>
+  </div>
 </template>
