82829af8e0b54f538641026c68f30375d01bcb6e
Dhiraj Bodicherla <dhiraj@gitlab.com>
2019-10-08 22:24:48 +0000

    feat: Provide way to hide dropdown programmatically

--- tests/test_utils.js
+++ tests/test_utils.js
@@ -1,3 +1,7 @@
+// adopted this method from Bootstraps utils
+// https://github.com/bootstrap-vue/bootstrap-vue/blob/2fd03f0b1d0cc41f9930078ba8b1c16b10e4ac2f/tests/utils.js#L6
+export const waitForAnimationFrame = () => new Promise(resolve => requestAnimationFrame(resolve));
+
 export const getResetAnimationsCSS = () => `
   *, *::after, *::before {
     -webkit-transition: none !important;
