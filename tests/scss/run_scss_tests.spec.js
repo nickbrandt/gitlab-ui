df71269c3c9750262406db55a9aea058ce9e9d06
jboyson1 <jboyson@gitlab.com>
2019-11-04 21:26:40 -0600

    chore: use functions for box-shadow
    This commit does the followinj:
    
    Introduce a new 'functions' folder and moves
    the existing px_to_rem file to that folder.
    
    Updates the scss tests to iterate over all files
    under tests/scss/functions.
    
    Add box_shadows.scss with corresponding
    box-shadow functions.
    
    Update new_button to use the new box_shadow
    functions.

--- tests/scss/run_scss_tests.spec.js
+++ tests/scss/run_scss_tests.spec.js
@@ -2,10 +2,21 @@ import sassTrue from 'sass-true';
 import path from 'path';
 import fs from 'fs';
 
-const testFiles = fs
+const endsWithScss = file => file.endsWith('.scss');
+const resolveRoot = file => path.resolve(__dirname, file);
+const resolveFunctions = file => path.resolve(`${__dirname}/functions`, file);
+
+const rootFiles = fs
   .readdirSync(__dirname)
-  .filter(file => file.endsWith('.scss'))
-  .map(file => path.resolve(__dirname, file));
+  .filter(endsWithScss)
+  .map(resolveRoot);
+
+const functionFiles = fs
+  .readdirSync(`${__dirname}/functions`)
+  .filter(endsWithScss)
+  .map(resolveFunctions);
+
+const testFiles = rootFiles.concat(functionFiles);
 
 testFiles.forEach(file => {
   sassTrue.runSass(
