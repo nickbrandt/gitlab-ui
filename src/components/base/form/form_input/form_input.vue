ab405db1a55ad578077f92392b07aae5de71e737
Paul Gascou-Vaillancourt <pgascouvaillancourt@gitlab.com>
2019-11-07 16:57:39 +0000

    fix(datepicker): fix form input and datepicker styles
    This fixes GitLab integration issues in datepicker components. Fixing
    these issues required to also fix some issues in the form input
    component.

--- src/components/base/form/form_input/form_input.vue
+++ src/components/base/form/form_input/form_input.vue
@@ -10,5 +10,5 @@ export default {
 </script>
 
 <template>
-  <b-form-input v-bind="$attrs" v-on="$listeners" />
+  <b-form-input class="gl-form-input" v-bind="$attrs" v-on="$listeners" />
 </template>
