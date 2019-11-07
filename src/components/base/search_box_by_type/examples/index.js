010c1a1ac02091eec886851fee9e2b3659d45215
Illya Klymov <xanf@xanf.me>
2019-11-07 15:21:56 +0000

    feat(search): Update search box by type component styling
    - Uses new GlIcon component
    - Proper styling

--- src/components/base/search_box_by_type/examples/index.js
+++ src/components/base/search_box_by_type/examples/index.js
@@ -1,4 +1,5 @@
 import SearchBoxByTypeDefaultExample from './search_box_by_type.default.example.vue';
+import SearchBoxByTypeLoadingExample from './search_box_by_type.loading.example.vue';
 
 export default [
   {
@@ -8,6 +9,11 @@ export default [
         id: 'search-box-by-type',
         name: 'default',
         component: SearchBoxByTypeDefaultExample,
+      },
+      {
+        id: 'search-box-by-type-loading',
+        name: 'loading',
+        component: SearchBoxByTypeLoadingExample,
       },
     ],
   },
