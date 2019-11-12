1939edaeaa1465344efea5b51d83170455dd1a53
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2019-11-12 13:28:12 -0500

    feat(form input): drop underlying BootstrapVue component
    The BootstrapVue component on which GlFormInput relies doesn't bring any
    value to our own component anymore and can be dropped to reduce the
    overhead and to make it easier to style the input without having to deal
    with Bootstrap class being applied by BootstrapVue

--- src/components/base/form/form_input/form_input.vue
+++ src/components/base/form/form_input/form_input.vue
@@ -1,14 +1,9 @@
 <script>
-import BFormInput from 'bootstrap-vue/src/components/form-input/form-input';
-
 export default {
-  components: {
-    BFormInput,
-  },
   inheritAttrs: false,
 };
 </script>
 
 <template>
-  <b-form-input class="gl-form-input" v-bind="$attrs" v-on="$listeners" />
+  <input type="text" class="gl-form-input" v-bind="$attrs" v-on="$listeners" />
 </template>
