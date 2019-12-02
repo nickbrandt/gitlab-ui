da4ad2946e50b47d57e16978bdff38a78ec3630a
pburdette <pburdette@gitlab.com>
2019-12-02 11:30:03 -0500

    refactor: change skeleton loader examples
    UX requested changes for the skeleton
    loader component. Some changes were
    made to the examples to better align
    with product examples of using the
    skeleton loader component.

--- src/components/base/skeleton_loader/examples/job_log_skeleton.basic.example.vue
+++ src/components/base/skeleton_loader/examples/job_log_skeleton.basic.example.vue
@@ -1,11 +1,15 @@
 <template>
-  <div style="width: 830px; height: 115px;">
-    <gl-skeleton-loader :width="830" :height="115">
-      <rect width="540" height="16" rx="4" />
-      <rect y="50" width="701" height="16" rx="4" />
-      <rect y="25" width="830" height="16" rx="4" />
-      <rect y="74" width="540" height="16" rx="4" />
-      <rect y="99" width="639" height="16" rx="4" />
-    </gl-skeleton-loader>
+  <div
+    style="background: #000; padding: 16px; border-radius: 0.25rem; width: 865px; height: 175px;"
+  >
+    <div style="width: 830px; height: 113px;">
+      <gl-skeleton-loader :width="830" :height="113">
+        <rect width="540" height="16" rx="4" />
+        <rect y="49" width="701" height="16" rx="4" />
+        <rect y="24" width="830" height="16" rx="4" />
+        <rect y="73" width="540" height="16" rx="4" />
+        <rect y="97" width="639" height="16" rx="4" />
+      </gl-skeleton-loader>
+    </div>
   </div>
 </template>
