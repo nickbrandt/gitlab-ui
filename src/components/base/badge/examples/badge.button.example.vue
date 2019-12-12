b78b4976d2e47e4d7c37393d460fc134c07edbf6
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2019-12-12 08:32:48 -0500

    revert: feat: add and style badges
    This reverts commit 4edcb0c27c987c7681bfadb5acc7f0a139577ee0.

--- src/components/base/badge/examples/badge.button.example.vue
+++ src/components/base/badge/examples/badge.button.example.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="text-center">
-    <gl-button variant="primary"> To-Do <gl-badge>42</gl-badge> </gl-button>
+    <gl-button variant="primary"> To-Do <gl-badge variant="light">42</gl-badge> </gl-button>
   </div>
 </template>
