bda036daa5921990b15d47f2fae48b42a53b2a50
Simon Knox <simon@gitlab.com>
2020-02-05 07:14:12 +1100

    Add novalidate example

--- src/components/base/form/examples/index.js
+++ src/components/base/form/examples/index.js
@@ -1,6 +1,7 @@
 import FormExample from './form.basic.example.vue';
 import InlineFormExample from './form.inline.example.vue';
 import EditFormExample from './form.edit.example.vue';
+import NoValidateFormExample from './form.novalidate.example.vue';
 
 export default [
   {
@@ -20,6 +21,11 @@ export default [
         id: 'form-edit',
         name: 'Form with existing data',
         component: EditFormExample,
+      },
+      {
+        id: 'form-novalidate',
+        name: 'Form with HTML validation disabled',
+        component: NoValidateFormExample,
       },
     ],
   },
