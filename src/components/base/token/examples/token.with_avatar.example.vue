1f38213265201142557b5bdd2c4e4af19c638b7b
Enrique Alcantara <ealcantara@gitlab.com>
2020-02-17 15:11:30 -0400

    refactor: Fix avatar usage in token example

--- src/components/base/token/examples/token.with_avatar.example.vue
+++ src/components/base/token/examples/token.with_avatar.example.vue
@@ -1,7 +1,3 @@
 <template>
-  <gl-token
-    ><gl-avatar
-      src="https://about.gitlab.com/images/press/gitlab-summit-south-africa.jpg"
-    />Token</gl-token
-  >
+  <gl-token><gl-avatar src="https://picsum.photos/id/1005/16" :size="16" />Token</gl-token>
 </template>
