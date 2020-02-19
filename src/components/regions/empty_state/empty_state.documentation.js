a87836e1da0b5e5cd3d71ed34df2f893faf90070
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2020-02-19 16:23:51 -0500

    docs(empty-state): document slots and props
    - Added missing documentation for the component's slots
    - Added more details about some of its props

--- src/components/regions/empty_state/empty_state.documentation.js
+++ src/components/regions/empty_state/empty_state.documentation.js
@@ -4,4 +4,24 @@ import examples from './examples';
 export default {
   description,
   examples,
+  propsInfo: {
+    svgPath: {
+      additionalInfo: "The illustration's URL.",
+    },
+    compact: {
+      additionalInfo: 'Set to true to enable the compact layout.',
+    },
+  },
+  slots: [
+    {
+      name: 'description',
+      description:
+        "Use this slot to customize the empty state's description area. Overrides the `description` prop.",
+    },
+    {
+      name: 'actions',
+      description:
+        "Use this slot to customize the empty state's actions area, where the buttons are. Overrides button-related props: `primaryButtonLink`, `primaryButtonText`, `secondaryButtonLink`, `secondaryButtonText`.",
+    },
+  ],
 };
