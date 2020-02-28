fdd2928f618e521df63858800df57cdd703ba379
Emily Ring <ering@gitlab.com>
2020-02-28 16:10:00 -0500

    build: Added disabled form group example
    Added examples for disabled input with form_groups.
    Updated realted snapshots.

---
+++ src/components/base/form/form_group/examples/form_group/form_group.disabled.example.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  computed: {
+    disabled() {
+      return true;
+    },
+  },
+};
+</script>
+
+<template>
+  <gl-form-group
+    id="group-id"
+    label="Label Name"
+    label-size="sm"
+    description="This feature is disabled"
+    label-for="input1"
+  >
+    <gl-form-input id="input1" type="text" :disabled="disabled" value="Disabled" />
+  </gl-form-group>
+</template>
