83756be4f7029298d0c1e1f71d5f3fa2477bcda0
Emily Ring <ering@gitlab.com>
2020-03-13 12:29:53 -0400

    added search Icon

---
+++ src/components/shared_components/clear_icon_button/clear_icon_button.vue
@@ -0,0 +1,51 @@
+<script>
+import GlIcon from '../../base/icon/icon.vue';
+import GlTooltip from '../../../directives/tooltip';
+
+export default {
+  components: {
+    GlIcon,
+  },
+  directives: {
+    GlTooltip,
+  },
+  props: {
+    buttonClass: {
+      type: String,
+      required: false,
+      default: 'gl-clear-icon-button',
+    },
+    value: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    tooltipContainer: {
+      required: false,
+      default: false,
+      validator: value =>
+        value === false || typeof value === 'string' || value instanceof HTMLElement,
+    },
+  },
+  computed: {
+    hasValue() {
+      return Boolean(this.value.length);
+    },
+  },
+  methods: {
+    buttonClicked() {
+      this.$emit('button-clicked');
+    }
+  }
+};
+</script>
+
+<template>
+  <button
+    v-if="hasValue"
+    :class="buttonClass"
+    @click="buttonClicked"
+  >
+    <gl-icon name="clear" />
+  </button>
+</template>
