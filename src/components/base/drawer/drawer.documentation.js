d2daf205073e9c9f5f8031022d1d357f9dc399b0
Paul Gascou-Vaillancourt <pgascouvaillancourt@gitlab.com>
2020-03-13 21:33:02 +0000

    chore(drawer): set followsDesignSystem to false

--- src/components/base/drawer/drawer.documentation.js
+++ src/components/base/drawer/drawer.documentation.js
@@ -2,7 +2,7 @@ import description from './drawer.md';
 import examples from './examples';
 
 export default {
-  followsDesignSystem: true,
+  followsDesignSystem: false,
   description,
   examples,
 };
