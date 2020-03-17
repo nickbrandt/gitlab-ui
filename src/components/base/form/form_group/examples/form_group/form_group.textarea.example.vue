1dbfafa2546a8ddc0a53ac4e81b15fa5c014e2f5
Emily Ring <ering@gitlab.com>
2020-03-17 09:43:27 -0400

    chore: Add additional styles to textarea

--- src/components/base/form/form_group/examples/form_group/form_group.textarea.example.vue
+++ src/components/base/form/form_group/examples/form_group/form_group.textarea.example.vue
@@ -13,7 +13,5 @@ export default {
     <gl-form-group id="group-id-textarea1" label="Label Name" label-for="textarea1">
       <gl-form-textarea id="textarea1" v-model="text" placeholder="Enter something" size="md" />
     </gl-form-group>
-
-    <p class="mt-3">The entered text is: {{ text }}</p>
   </div>
 </template>
