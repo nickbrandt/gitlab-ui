463a0da21e6ebe59479fe3e16dd340a61302a972
Simon Knox <simon@gitlab.com>
2020-04-08 16:24:59 +1000

    Replace engineeringNotation with Intl.NumberFormat

--- src/utils/number_utils.js
+++ src/utils/number_utils.js
@@ -25,62 +25,13 @@ export const isIntGreaterThan = gt => number => Number.isInteger(number) && numb
 
 /**
  * Convert number to engineering format, using SI suffix
- * @param {Number|String} num
- * @param {Number} precision
+ * @param {Number} num
  * @return {String} number, possibly with a suffix
  */
-export const engineeringNotation = (num, precision = 2) => {
-  let validNum = num;
-  const invalidValues = [NaN, Infinity, -Infinity];
+export const engineeringNotation = (num) => {
+  const formatter = new Intl.NumberFormat('en-US', {
+    notation: 'engineering',
+  });
 
-  // ensure that we're handling strings that contain comma / decimal variations
-  if (typeof validNum === 'string' && (validNum.includes(',') || validNum.includes('.'))) {
-    validNum = parseFloat(validNum.replace(',', ''));
-  }
-
-  if (invalidValues.includes(Number(validNum)) || invalidValues.includes(Number(precision))) {
-    return validNum.toString();
-  }
-
-  const allYourBase = {
-    '-24': 'y',
-    '-21': 'z',
-    '-18': 'a',
-    '-15': 'f',
-    '-12': 'p',
-    '-9': 'n',
-    '-6': 'μ',
-    '-3': 'm',
-    '0': '',
-    '3': 'k',
-    '6': 'M',
-    '9': 'G',
-    '12': 'T',
-    '15': 'P',
-    '18': 'E',
-    '21': 'Z',
-    '24': 'Y',
-  };
-
-  const exponentialNotation = validNum.toExponential(precision);
-
-  const power = exponentialNotation.split('e').map(Number)[1] || 0;
-
-  if (power < -24 || power > 24) {
-    return exponentialNotation;
-  }
-
-  const scaledPower = 3 * Math.floor(power / 3);
-  const scaledMantissa = (exponentialNotation / 10 ** scaledPower)
-    // strip trailing decimals from floating point rounding errors
-    .toFixed(precision)
-    // strip trailing 0s after a decimal point
-    .replace(/\.([1-9]*)0+$/, (_, fractionalDigits) => {
-      if (fractionalDigits) {
-        return `.${fractionalDigits}`;
-      }
-      return '';
-    });
-
-  return `${scaledMantissa}${allYourBase[scaledPower]}`;
+  return formatter.format(num);
 };
