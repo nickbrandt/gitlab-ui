d26f397a078f07a2d0a3a5e0085acded2a2cd9f2
Vitaly Slobodin <vslobodin@gitlab.com>
2020-04-15 14:45:01 +0300

    refactor: 💡 Remove deprecated number sizes from GlLoadingIcon
    Passing size as number (:size="2") has been deprecated.
    All cases with deprecated sizes have been updated.
    
    BREAKING CHANGE: 🧨 GlLoadingIcon does not accept size as a number.
    Use string instead.

--- src/components/base/loading_icon/loading_icon.vue
+++ src/components/base/loading_icon/loading_icon.vue
@@ -1,23 +1,15 @@
 <script>
-const sizes = {
-  sm: 'sm', // -> 16px
-  md: 'md', // -> 24px
-  lg: 'lg', // -> 32px
-  xl: 'xl', // -> 64px
-};
-const sizeToStringMap = {
-  1: sizes.sm,
-  2: sizes.lg,
-  3: sizes.lg,
-  4: sizes.xl,
-  5: sizes.xl,
-};
+const sizes = [
+  'sm', // -> 16px
+  'md', // -> 24px
+  'lg', // -> 32px
+  'xl', // -> 64px
+];
 const colors = {
   orange: 'orange',
   dark: 'dark',
   light: 'light',
 };
-const defaultSize = sizes.sm;
 const defaultColor = colors.orange;
 
 const baseCssClass = 'gl-spinner';
@@ -30,11 +22,11 @@ export default {
       default: 'Loading',
     },
     size: {
-      type: [Number, String],
+      type: String,
       required: false,
-      default: defaultSize,
+      default: 'sm',
       validator(value) {
-        return (value > 0 && value < 6) || Object.keys(sizes).includes(value);
+        return sizes.indexOf(value) !== -1;
       },
     },
     color: {
@@ -55,26 +47,13 @@ export default {
     rootElementType() {
       return this.inline ? 'span' : 'div';
     },
-    sizeToString() {
-      return sizeToStringMap[this.size] || defaultSize;
-    },
-    isSizeString() {
-      return Number.isNaN(parseInt(this.size, 10));
-    },
-    actualSize() {
-      return this.isSizeString ? sizes[this.size] : this.sizeToString;
-    },
     cssClasses() {
       return [
         baseCssClass,
         `${baseCssClass}-${colors[this.color]}`,
-        `${baseCssClass}-${sizes[this.actualSize]}`,
+        `${baseCssClass}-${this.size}`,
       ];
     },
-  },
-  created() {
-    if (!this.isSizeString)
-      console.warn("Icon sizes 1 - 5 are deprecated, please use 'sm', 'md' and 'lg' instead."); // eslint-disable-line no-console
   },
 };
 </script>
