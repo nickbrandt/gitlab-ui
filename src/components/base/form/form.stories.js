70d15c368219595cdd40ee1f27725878dc981029
David O'Regan <doregan@gitlab.com>
2020-05-26 14:42:05 +0000

    docs(GlForm): Add GlForm stories

---
+++ src/components/base/form/form.stories.js
@@ -0,0 +1,17 @@
+import { withKnobs } from '@storybook/addon-knobs/vue';
+import { documentedStoriesOf } from '../../../../documentation/documented_stories';
+import readme from './form.md';
+import { GlForm } from '../../../../index';
+
+const components = {
+  GlForm,
+};
+
+documentedStoriesOf('base|form/form', readme)
+  .addDecorator(withKnobs)
+  .add('default', () => ({
+    components,
+    template: `
+      <gl-form />
+    `,
+  }));
