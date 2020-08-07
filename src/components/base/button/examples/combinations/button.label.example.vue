c7df4039791915e076d23963ab7d6aaf3c380cc8
jeldergl <jelder@gitlab.com>
2020-08-07 10:36:55 -0500

    feat(Button): Use monospace for one label button example

--- src/components/base/button/examples/combinations/button.label.example.vue
+++ src/components/base/button/examples/combinations/button.label.example.vue
@@ -3,7 +3,7 @@
     <gl-button label>Label</gl-button>
     <gl-button size="small" label>Label</gl-button>
     <gl-button-group>
-      <gl-button label>b29cc44d</gl-button>
+      <gl-button label class="gl-font-monospace">b29cc44d</gl-button>
       <gl-button icon="duplicate" />
       <gl-button icon="folder-open" />
     </gl-button-group>
