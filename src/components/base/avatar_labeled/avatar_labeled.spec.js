7dffcd8fd501efb75f49158fbd012d03937fc5de
peterhegman <phegman@gitlab.com>
2020-09-15 13:06:46 -0700

    feat(GLAvatarLabeled): Add slot for meta data
    New slot can be used for adding badges, user status emoji, and other
    meta data to the avatar
    
    Also added example with badges

--- src/components/base/avatar_labeled/avatar_labeled.spec.js
+++ src/components/base/avatar_labeled/avatar_labeled.spec.js
@@ -6,12 +6,16 @@ import AvatarLabeled from './avatar_labeled.vue';
 describe('avatar labeled', () => {
   let wrapper;
 
-  const buildWrapper = propsData => {
+  const label = 'avatar label';
+  const subLabel = 'avatar label';
+
+  const buildWrapper = (propsData, slots) => {
     wrapper = shallowMount(AvatarLabeled, {
       propsData: {
         label: '',
         ...propsData,
       },
+      slots,
     });
   };
 
@@ -26,18 +30,23 @@ describe('avatar labeled', () => {
   });
 
   it('displays the avatar label', () => {
-    const label = 'avatar label';
-
     buildWrapper({ label });
 
     expect(wrapper.find('.gl-avatar-labeled-label').text()).toEqual(label);
   });
 
   it('displays the avatar sub label', () => {
-    const subLabel = 'avatar label';
-
     buildWrapper({ subLabel });
 
     expect(wrapper.find('.gl-avatar-labeled-sublabel').text()).toEqual(subLabel);
+  });
+
+  it('displays `meta` slot', () => {
+    buildWrapper(
+      { label, subLabel },
+      { meta: '<span data-testid="user-meta" class="gl-p-1">Foo Bar</span>' }
+    );
+
+    expect(wrapper.find('[data-testid="user-meta"]').exists()).toBe(true);
   });
 });
