7ecad5494ec4f288598bd073f667448e35204141
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2021-01-16 13:53:59 -0500

    chore: setup Storybook addons
    Install and enables the following addons:
    - @storybook/addon-docs
    - @storybook/addon-storysource

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,8 @@
 module.exports = {
   stories: ['../src/**/*.stories.js'],
   addons: [
+    '@storybook/addon-docs',
+    '@storybook/addon-storysource',
     '@storybook/addon-knobs',
     '@storybook/addon-a11y',
     '@storybook/addon-viewport/register',
