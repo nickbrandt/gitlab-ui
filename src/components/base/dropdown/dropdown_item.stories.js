88fe7e619041e801751a08658ec6c181f8f57a48
Paul Slaughter <pslaughter@gitlab.com>
2021-01-21 01:20:50 -0600

    fix(GlDropdown): Uncenter dropdown item check
    It doesn't look right for the checkbox to be centered
    Let's align-self start this and add top spacing that
    is consistent with the text wrapper.

--- src/components/base/dropdown/dropdown_item.stories.js
+++ src/components/base/dropdown/dropdown_item.stories.js
@@ -13,4 +13,30 @@ documentedStoriesOf('base/dropdown/dropdown-item', readme)
     props: {},
     components,
     template: '<ul class="list-unstyled"><gl-dropdown-item>Some item</gl-dropdown-item></ul>',
+  }))
+  .add('checked', () => ({
+    props: {},
+    components,
+    template:
+      '<ul class="list-unstyled"><gl-dropdown-item :is-checked="true" :is-check-item="true">Some item</gl-dropdown-item></ul>',
+  }))
+  .add('checked with avatar', () => ({
+    props: {},
+    components,
+    template: `<ul class="list-unstyled">
+      <gl-dropdown-item
+        :is-checked="true"
+        :is-check-item="true"
+        :is-check-centered="true"
+        avatar-url="https://secure.gravatar.com/avatar/78b060780d36f51a6763ac9831a4f022?s=180&d=identicon"
+        secondary-text="@sytses"
+      >
+        Sid Sijbrandij
+      </gl-dropdown-item></ul>`,
+  }))
+  .add('checked with secondary text', () => ({
+    props: {},
+    components,
+    template:
+      '<ul class="list-unstyled"><gl-dropdown-item :is-checked="true" :is-check-item="true" secondary-text="Lorem ipsum dolar sit amit...">Some item</gl-dropdown-item></ul>',
   }));
