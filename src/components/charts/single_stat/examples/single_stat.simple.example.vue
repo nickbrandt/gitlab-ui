eac46dbc3c35909863bb817bcab57402875563c9
Brandon Labuschagne <blabuschagne@gitlab.com>
2021-02-17 06:21:29 +0000

    fix(GlSingleStat): Update component examples
    This commit makes the GlSingleStat component examples
    match the design system specifications.

--- src/components/charts/single_stat/examples/single_stat.simple.example.vue
+++ src/components/charts/single_stat/examples/single_stat.simple.example.vue
@@ -1,13 +1,7 @@
 <template>
   <div class="row">
     <div class="col-sm-4">
-      <gl-single-stat title="Success stat" value="100" unit="ms" variant="success" />
-    </div>
-    <div class="col-sm-4">
-      <gl-single-stat title="Warning stat" value="1000" unit="ms" variant="warning" />
-    </div>
-    <div class="col-sm-4">
-      <gl-single-stat title="Danger stat" value="100000" unit="ms" variant="danger" />
+      <gl-single-stat title="Single stat" value="100" />
     </div>
   </div>
 </template>
