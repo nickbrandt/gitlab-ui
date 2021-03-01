8825c63836be9ce5498cdafd7ea20ed2cb0bc18e
Jeremy Elder <jelder@gitlab.com>
2021-03-01 09:25:57 -0600

    chore: Update followsDesignSystem to true

--- src/components/base/alert/alert.documentation.js
+++ src/components/base/alert/alert.documentation.js
@@ -4,7 +4,7 @@ import examples from './examples';
 export default {
   description,
   examples,
-  followsDesignSystem: false,
+  followsDesignSystem: true,
   propsInfo: {
     primaryButtonText: {
       additionalInfo: 'If provided, renders a primary action button.',
