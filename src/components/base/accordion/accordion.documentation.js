0a849f0e2a2229545775bde005844a9023be44ba
Mike Greiling <mike@pixelcog.com>
2021-03-26 16:13:36 -0500

    chore(GlAccordion): Update documentation

--- src/components/base/accordion/accordion.documentation.js
+++ src/components/base/accordion/accordion.documentation.js
@@ -6,4 +6,14 @@ export default {
   description,
   examples,
   bootstrapComponent: 'b-collapse',
+  propsInfo: {
+    autoCollapse: {
+      additionalInfo:
+        'When true, will have the effect of closing other accordion items when one accordion item is visible.',
+    },
+    headerLevel: {
+      additionalInfo:
+        'The header tag used in the accordion (h1/h2/h3/h4/h5/h6). This overrides the value provided by GlAccordion. For accessibility this should be set to an appropriate value in the context where the accordion is used.',
+    },
+  },
 };
--- src/components/base/accordion/accordion_item.documentation.js
+++ src/components/base/accordion/accordion_item.documentation.js
@@ -4,6 +4,7 @@ import examples from './examples';
 export default {
   description,
   examples,
+  bootstrapComponent: 'b-collapse',
   propsInfo: {
     title: {
       additionalInfo: 'Used to set the title of accordion link.',
@@ -11,13 +12,9 @@ export default {
     visible: {
       additionalInfo: 'When set, it will ensure the accordion item is initially visible.',
     },
-    accordion: {
-      additionalInfo:
-        'The name of the accordion group that this accordion item belongs to. Supplying an accordion group identifier will provide the effect of closing other accordion item when one accordion item is visible.',
-    },
     headerLevel: {
       additionalInfo:
-        'The header tag used in the accordion (h1/h2/h3/h4/h5/h6). For accessibility this should be set to an appropriate value in the context where the accordion is used.',
+        'The header tag used in the accordion (h1/h2/h3/h4/h5/h6). This overrides the value provided by GlAccordion. For accessibility this should be set to an appropriate value in the context where the accordion is used.',
     },
   },
 };
