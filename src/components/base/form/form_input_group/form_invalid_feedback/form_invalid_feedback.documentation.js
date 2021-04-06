7f3dbab80cceab5d30e413e91028fc6fca508c3b
Samantha Ming <sming@gitlab.com>
2021-04-05 20:01:26 -0700

    Remove component examples
    Examples are moved to design.gitlab.com as a separate MR.

--- src/components/base/form/form_input_group/form_invalid_feedback/form_invalid_feedback.documentation.js
+++ src/components/base/form/form_input_group/form_invalid_feedback/form_invalid_feedback.documentation.js
@@ -1,8 +1,6 @@
-import examples from './examples';
 import * as description from './form_invalid_feedback.md';
 
 export default {
   description,
-  examples,
   bootstrapComponent: 'b-form-invalid-feedback',
 };
