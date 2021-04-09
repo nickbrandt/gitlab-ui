0cf64385c7a7d0a8cde5ffb6ff7a974ba443550b
jboyson <jboyson@gitlab.com>
2021-04-09 12:55:08 -0500

    fix: only remove padding for disimissable token

--- src/components/base/token/token.vue
+++ src/components/base/token/token.vue
@@ -21,7 +21,9 @@ export default {
   },
   computed: {
     variantClass() {
-      return `gl-token-${this.variant}-variant`;
+      let classes = `gl-token-${this.variant}-variant`;
+      if (!this.viewOnly) classes += ' gl-pt-0! gl-pr-0! gl-pb-0!';
+      return classes;
     },
   },
   methods: {
@@ -33,7 +35,7 @@ export default {
 </script>
 
 <template>
-  <span :class="['gl-token gl-pt-0! gl-pr-0! gl-pb-0!', variantClass]" v-on="$listeners">
+  <span :class="['gl-token', variantClass]" v-on="$listeners">
     <span class="gl-token-content">
       <slot></slot>
       <close-button v-if="!viewOnly" class="gl-token-close" label="close" @click="close" />
