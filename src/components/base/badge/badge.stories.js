5db838e11fa34e4c72bcd682b848e37944e50363
Tobias Weichart <tobias.weichart@gmail.com>
2021-04-13 21:43:35 +0000

    chore(GlBadge): use original example component

--- src/components/base/badge/badge.stories.js
+++ src/components/base/badge/badge.stories.js
@@ -61,7 +61,7 @@ const ButtonBadgeStory = (args, { argTypes }) => ({
   template: `
     <gl-button category="primary" variant="info">
       To-Do
-      ${template}
+      <gl-badge class="gl-ml-2">42</gl-badge>
     </gl-button>`,
 });
 
