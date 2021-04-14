e9727a49cb16c5d43d0edd643711bc885524c236
Thomas Randolph <trandolph@gitlab.com>
2021-04-14 20:39:52 +0000

    docs: Clarify why returning a fragment from dompurify is preferable

--- src/directives/safe_html/safe_html.js
+++ src/directives/safe_html/safe_html.js
@@ -1,5 +1,10 @@
 import { sanitize } from 'dompurify';
 
+// Mitigate against future dompurify mXSS bypasses by
+// avoiding additional serialize/parse round trip.
+// See https://gitlab.com/gitlab-org/gitlab-ui/-/merge_requests/1782
+// and https://gitlab.com/gitlab-org/gitlab-ui/-/merge_requests/2127
+// for more details.
 const DEFAULT_CONFIG = { RETURN_DOM_FRAGMENT: true };
 
 const transform = (el, binding) => {
