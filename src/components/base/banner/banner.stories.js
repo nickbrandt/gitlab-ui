56c4cdad4be518cdac6193225c778264cb1b9bf8
Tobias Weichart <tobias.weichart@gmail.com>
2021-04-27 11:40:06 +0100

    chore(GlBanner): change to use general Template variable

--- src/components/base/banner/banner.stories.js
+++ src/components/base/banner/banner.stories.js
@@ -1,8 +1,20 @@
 import clusterPopoverSvg from '@gitlab/svgs/dist/illustrations/cluster_popover.svg';
 import serviceDeskCalloutSvg from '@gitlab/svgs/dist/illustrations/service_desk_callout.svg';
-import { GlBanner, GlLink, GlButton } from '../../../../index';
+import { GlBanner, GlButton } from '../../../../index';
 import { bannerVariants } from '../../../utils/constants';
 import readme from './banner.md';
+
+const template = `
+    <gl-banner
+      :title="title"
+      :button-text="buttonText"
+      :button-link="buttonLink"
+      :svg-path="svgPath"
+      :variant="variant"
+    >
+      <p>GitLab Service Desk is a simple way to allow people to create issues in your GitLab instance without needing their own user account. It provides a unique email address for end users to create issues in a project, and replies can be sent either though the GitLab interface or by email. End users will only see the thread though email.</p>
+    </gl-banner>
+`;
 
 const generateProps = ({
   title = 'Upgrade your plan to activate Service Desk',
@@ -18,73 +30,26 @@ const generateProps = ({
   variant,
 });
 
-const DefaultStory = (args, { argTypes }) => ({
+const Template = (args, { argTypes }) => ({
   components: {
     GlBanner,
   },
   props: Object.keys(argTypes),
-  template: `
-    <gl-banner
-      :title="title"
-      :button-text="buttonText"
-      :button-link="buttonLink"
-      :svg-path="svgPath"
-      :variant="variant"
-    >
-      <p>GitLab Service Desk is a simple way to allow people to create issues in your GitLab instance without needing their own user account. It provides a unique email address for end users to create issues in a project, and replies can be sent either though the GitLab interface or by email. End users will only see the thread though email.</p>
-    </gl-banner>`,
+  template,
 });
-
-const Default = DefaultStory.bind({});
+export const Default = Template.bind({});
 Default.args = generateProps();
 
-const NoImageStory = (args, { argTypes }) => ({
-  components: {
-    GlBanner,
-    GlLink,
-  },
-  props: Object.keys(argTypes),
-  template: `
-      <gl-banner
-        title="Auto DevOps (Beta)"
-        button-text="Upgrade your plan"
-        :button-link="buttonLink"
-        svg-path="${clusterPopoverSvg}"
-        variant="${bannerVariants[1]}"
-      >
-       <p>Auto DevOps can be enabled for this project. It will automatically build, test, and deploy your application based on a predefined CI/CD configuration.</p>
-       <p>Learn more in the <gl-link href="#">Auto DevOps documentation</gl-link>.</p>
-      </gl-banner>`,
+export const NoImage = Template.bind({});
+NoImage.args = generateProps({
+  svgPath: null,
 });
-const NoImage = NoImageStory.bind({});
-NoImage.args = generateProps();
-NoImage.parameters = {
-  storyshots: { disable: true },
-};
 
-const IntroductionStory = (args, { argTypes }) => ({
-  components: {
-    GlBanner,
-    GlLink,
-  },
-  props: Object.keys(argTypes),
-  template: `
-      <gl-banner
-          title="Auto DevOps (Beta)"
-          button-text="Upgrade your plan"
-          :button-link="buttonLink"
-          svg-path="${clusterPopoverSvg}"
-          variant="${bannerVariants[1]}"
-      >
-      <p>Auto DevOps can be enabled for this project. It will automatically build, test, and deploy your application based on a predefined CI/CD configuration.</p>
-      <p>Learn more in the <gl-link href="#">Auto DevOps documentation</gl-link>.</p>
-      </gl-banner>`,
+export const Introduction = Template.bind({});
+Introduction.args = generateProps({
+  svgPath: clusterPopoverSvg,
+  variant: bannerVariants[1],
 });
-const Introduction = IntroductionStory.bind({});
-Introduction.args = generateProps();
-Introduction.parameters = {
-  storyshots: { disable: true },
-};
 
 const WithActionsStory = (args, { argTypes }) => ({
   components: {
@@ -93,19 +58,25 @@ const WithActionsStory = (args, { argTypes }) => ({
   },
   props: Object.keys(argTypes),
   template: `
-      <gl-banner
-          title="Button with actions banner"
-          button-text="Primary Button"
-          :button-link="buttonLink"
-      >
+    <gl-banner
+        :title="title"
+        :button-text="buttonText"
+        :button-link="buttonLink"
+        :svg-path="svgPath"
+        :variant="variant"
+    >
       <p>There should be a primary button and a link button below this text.</p>
       <template #actions>
         <gl-button class="gl-ml-3" variant="link">Ask again later</gl-button>
       </template>
       </gl-banner>`,
 });
-const WithActions = WithActionsStory.bind({});
-WithActions.args = generateProps();
+export const WithActions = WithActionsStory.bind({});
+WithActions.args = generateProps({
+  title: 'Button with actions banner',
+  buttonText: 'Primary Button',
+  svgPath: null,
+});
 WithActions.parameters = {
   storyshots: { disable: true },
 };
