c849f7d0e0356c6f0f756c3051619ff54f19f7e4
Justin Boyson <jboyson@gitlab.com>
2021-05-21 14:04:36 +0000

    fix(GlToken): use CloseButton instead of GlIcon

--- src/components/base/token/token.spec.js
+++ src/components/base/token/token.spec.js
@@ -1,13 +1,15 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils';
-import GlIcon from '../icon/icon.vue';
+import CloseButton from '../../shared_components/close_button/close_button.vue';
 import Token from './token.vue';
 
 const localVue = createLocalVue();
 
-const findIcon = (wrapper) => wrapper.findComponent(GlIcon);
+const findIcon = (wrapper) => wrapper.findComponent(CloseButton);
 
 describe('Token component', () => {
   let wrapper;
+
+  const viewOnlyClass = 'gl-token-view-only';
 
   const createComponent = (propsData) =>
     shallowMount(Token, {
@@ -30,8 +32,20 @@ describe('Token component', () => {
     expect(findIcon(wrapper).exists()).toBe(false);
   });
 
+  it('includes view-only class when view-only', () => {
+    wrapper = createComponent({ viewOnly: true });
+    expect(wrapper.classes()).toContain(viewOnlyClass);
+  });
+
+  it('does not include view-only class when not view-only', () => {
+    wrapper = createComponent({ viewOnly: false });
+    expect(wrapper.classes()).not.toContain(viewOnlyClass);
+  });
+
   it.each`
     variant           | cssClass
+    ${undefined}      | ${'gl-token-default-variant'}
+    ${'default'}      | ${'gl-token-default-variant'}
     ${'search-type'}  | ${'gl-token-search-type-variant'}
     ${'search-value'} | ${'gl-token-search-value-variant'}
   `('sets class .$cssClass when variant is $variant', ({ variant, cssClass }) => {
