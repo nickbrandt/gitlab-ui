c8b8bd7eed6b2bb702daf5f1a9e1005570bcd6ff
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2021-06-15 17:44:10 -0400

    require.context approach

--- .storybook/docs/blocks/LinkToSource.js
+++ .storybook/docs/blocks/LinkToSource.js
@@ -1,17 +1,24 @@
 import React, { useContext } from 'react';
+import { snakeCase } from 'lodash';
 import { DocsContext } from '@storybook/addon-docs/blocks';
 import { Button } from '@storybook/components';
 
 const BASE_URL = 'https://gitlab.com/gitlab-org/gitlab-ui/-/tree/main/src/components';
 
+const components = require.context('../../../src/components', true, /\.vue$/);
+
 export const LinkToSource = (props) => {
   const context = useContext(DocsContext);
-  const { kind: directory } = context;
-  if (!directory) {
+  const { kind } = context;
+  if (!kind) {
     return null;
   }
-  const filename = directory.replace(/.*\/([^/]+)$/, '$1');
-  const href = `${BASE_URL}/${directory}/${filename}.vue`;
+  const componentName = snakeCase(kind.replace(/.*\/([^/]+)$/, '$1'));
+  const filePath = components.keys().find((path) => path.endsWith(`/${componentName}.vue`));
+  if (!filePath) {
+    return null;
+  }
+  const href = `${BASE_URL}/${filePath.replace(/^\.\//, '')}`;
   return (
     <Button href={href} rel="noopener" target="_blank" isLink small {...props}>
       View source
