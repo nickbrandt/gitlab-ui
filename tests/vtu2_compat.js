5f9a7672f24b4fbd3c3d27f484b80e72500928a8
Illya Klymov <xanf@xanf.me>
2021-06-21 12:02:05 +0300

    chore(vue3): Add normalization of mount options to VTU2 compat
    - convert mount options to new format used in VTU@2

--- tests/vtu2_compat.js
+++ tests/vtu2_compat.js
@@ -1,8 +1,49 @@
+function normalizeMountArgs(args = {}) {
+  const {
+    propsData: props,
+    mocks,
+    provide,
+    directives,
+    components,
+    slots = {},
+    scopedSlots = {},
+    stubs,
+    ...restArgs
+  } = args;
+
+  return {
+    props,
+    global: {
+      stubs,
+      mocks,
+      provide,
+      directives,
+      components,
+    },
+    slots: {
+      ...slots,
+      ...scopedSlots,
+    },
+    ...restArgs,
+  };
+}
+
 module.exports = () => {
-  // requiring VTU is not side-effect free3, so defer it to real installation
+  // requiring VTU is not side-effect free, so defer it to real installation
   // eslint-disable-next-line global-require
   const VTU = require('@vue/test-utils-vue3');
 
   // destroy was renamed to unmount
   VTU.VueWrapper.prototype.destroy = VTU.VueWrapper.prototype.unmount;
+
+  // location of certain options changed for mount & shallowMount
+  const originalMount = VTU.mount;
+  VTU.mount = function patchedMount(component, args = {}) {
+    return originalMount.call(this, component, normalizeMountArgs(args));
+  };
+
+  const originalShallowMount = VTU.shallowMount;
+  VTU.shallowMount = function patchedMount(component, args = {}) {
+    return originalShallowMount.call(this, component, normalizeMountArgs(args));
+  };
 };
